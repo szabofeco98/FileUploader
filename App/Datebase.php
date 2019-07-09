6b08d6aae2c7ab3f9b6fe54cc4b0c2e82ccc774d
Szabo Ferenc <szabofeco98@gmail.com>
2019-07-09 12:23:05 +0200

    uploadForm

--- App/Datebase.php
+++ App/Datebase.php
@@ -2,8 +2,20 @@
 
 
 class Datebase{
+    static $db=null;
+
+    public function __construct(){
+    }
 
     public static function getDatabase(){
-       return new PDO("mysql:host=localhost;dbname=FileStorage","admin","admin4321");
+
+        if(is_null(self::$db)){
+             self::$db=new PDO("mysql:host=localhost;dbname=FileStorage","admin","admin4321");
+             self::$db->exec("set names utf8");
+
+        }
+        return self::$db;
+
+        //return new PDO("mysql:host=localhost;dbname=FileStorage","admin","admin4321");
     }
 }
--- App/controllers/FileManagerController.php
+++ App/controllers/FileManagerController.php
@@ -1,17 +1,33 @@
 <?php
 
 
-class FileManagerController{
+class FileManagerController extends BaseController {
+
     function __construct(){
         Session::init();
         if(!Session::get("loggedin")) {
             Session::destroy();
             autoload("LoginView");
         }else{
-            autoload("FileManagerView");
             echo "Hello ".Session::get("user_name");
+            autoload("FileManagerView");
         }
 
+    }
+
+    function upload(){
+        if(isset($_POST['submit'])){
+            $errors= array();
+            $file_name = $_FILES['fileToUpload']['name'];
+            $file_size =$_FILES['fileToUpload']['size'];
+            $file_tmp =$_FILES['fileToUpload']['tmp_name'];
+            $file_type=$_FILES['fileToUpload']['type'];
+            $file_ext=strtolower(end(explode('.',$_FILES['fileToUpload']['name'])));
+
+            echo $file_name."<br/>".$file_size."<br/>".$file_tmp."<br/>".$file_type;
+            move_uploaded_file($file_tmp,"/".$file_name);
+            echo "Success";
+        }
     }
 
 
