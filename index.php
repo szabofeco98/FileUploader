87d3cb2150efd22144a59a18f386fab6f493fa24
szabofeco98 <36442749+szabofeco98@users.noreply.github.com>
2019-10-16 14:16:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,10 @@
 
 
 ?>
+    $myfile=fopen("valami.txt", "r") or die("unable to open");
+	$readed=fread($myfile, filesize("valami.txt"));
+
+    $db = new PDO('mysql:host=localhost;dbname=test;',"root","");
+	$stmt=$db->prepare("SELECT valami from test where username=:userName");
+	$stmt->execute(["userName"=>$_POST['username']]);
+	$select=$stmt->fetch(PDO::FETCH_ASSOC);
